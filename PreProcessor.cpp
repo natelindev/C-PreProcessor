6136e40fbda29117a0d8f271a6e7c96a5ac58794
Nathaniel <linlilulll@gmail.com>
2017-05-08 15:22:48 +0800

    Bug fixes

--- PreProcessor.cpp
+++ PreProcessor.cpp
@@ -11,7 +11,7 @@
 #include <string>
 
 #ifndef DEBUG_ENABLE
-#define DEBUG_ENABLE true
+#define DEBUG_ENABLE false
 #endif
 
 using namespace std;
@@ -36,13 +36,17 @@ int main(int argc, const char * argv[]) {
                         //#include
                         if (it+7 < buffer.end()) {
                             if (string(it,it+7).compare("include")){
-                                cerr<<"#inlcude recognized at line "<<ctr<<endl;
+                                if(DEBUG_ENABLE){
+                                    cerr<<"#inlcude recognized at line "<<ctr<<endl;
+                                }
                                 //TODO: implement #inlcude here
                                 buffer_processed.clear();
                             }
                         }
                         else{
-                            cerr<<"Unknown command "<<string(it,find(it, buffer.end(),' '))<<" at line "<<ctr<<endl;
+                            if(DEBUG_ENABLE){
+                                cerr<<"Unknown command "<<string(it,find(it, buffer.end(),' '))<<" at line "<<ctr<<endl;
+                            }
                             buffer_processed.clear();
                         }
                     }
@@ -50,18 +54,24 @@ int main(int argc, const char * argv[]) {
                         //#define
                         if (it+6 < buffer.end()) {
                             if (string(it,it+6).compare("define")) {
-                                cerr<<"#define recognized at line "<<ctr<<endl;
+                                if(DEBUG_ENABLE){
+                                    cerr<<"#define recognized at line "<<ctr<<endl;
+                                }
                                 //TODO: implement #define here
                                 buffer_processed.clear();
                             }
                         }
                         else{
-                            cerr<<"Unknown command "<<string(it,find(it, buffer.end(),' '))<<" at line "<<ctr<<endl;
+                            if(DEBUG_ENABLE){
+                                cerr<<"Unknown command "<<string(it,find(it, buffer.end(),' '))<<" at line "<<ctr<<endl;
+                            }
                             buffer_processed.clear();
                         }
                     }
                     else{
-                        cerr<<"Unknown command "<<string(it,find(it, buffer.end(),' '))<<" at line "<<ctr<<endl;
+                        if(DEBUG_ENABLE){
+                            cerr<<"Unknown command "<<string(it,find(it, buffer.end(),' '))<<" at line "<<ctr<<endl;
+                        }
                         buffer_processed.clear();
                     }
                 }
@@ -70,24 +80,30 @@ int main(int argc, const char * argv[]) {
                 if (it+1 < buffer.end()) {
                     if (*(it+1) == '/') {
                         //line comment
-                        cerr<<"Line comment recognized at line "<<ctr<<endl;
+                        if(DEBUG_ENABLE){
+                            cerr<<"Line comment recognized at line "<<ctr<<endl;
+                        }
                         buffer_processed = string(buffer.begin(),it);
                     }
                     else if(*(it+1) == '*'){
                         //block comment
-                        cerr<<"Block comment begin at line "<<ctr<<endl;
+                        if(DEBUG_ENABLE){
+                            cerr<<"Block comment begin at line "<<ctr<<endl;
+                        }
                         buffer_processed = string(buffer.begin(),it);
                         in_block_comment = true;
                         block_comment_begin = true;
                     }
-
+                    
                 }
             }
             else if (in_block_comment && *it == '*'){
                 if (it+1 < buffer.end()) {
                     if (*(it+1) == '/') {
                         //block comment end
-                        cerr<<"Block comment end at line "<<ctr<<endl;
+                        if(DEBUG_ENABLE){
+                            cerr<<"Block comment end at line "<<ctr<<endl;
+                        }
                         in_block_comment = false;
                         buffer_processed = string(it+2,buffer.end());
                     }
